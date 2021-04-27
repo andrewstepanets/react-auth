187eb41028f9d5a67cd21c8b9a262f3a3e04c9e6
Andrii Stepanets <ovrass@gmail.com>
2021-04-27 14:26:51 +0300

    refactor: added components and styling its

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 // import { NavBar, Footer, Loading } from "./components";
-import { NavBar } from "./components";
-// import { Home, Profile, ExternalApi } from "./views";
-// import ProtectedRoute from "./auth/protected-route";
+import { NavBar, Loading } from "./components";
+import { Home, Profile, ExternalApi } from "./pages";
+import ProtectedRoute from "./auth/protected-route";
 
 import "./App.css";
 
@@ -13,7 +13,7 @@ const App = () => {
   const { isLoading } = useAuth0();
 
   if (isLoading) {
-    // return <Loading />;
+    return <Loading />;
   }
 
   return (
@@ -21,9 +21,9 @@ const App = () => {
       <NavBar />
       <div className="container flex-grow-1">
         <Switch>
-          {/* <Route path="/" exact component={Home} />
+          <Route path="/" exact component={Home} />
           <ProtectedRoute path="/profile" component={Profile} />
-          <ProtectedRoute path="/external-api" component={ExternalApi} /> */}
+          <ProtectedRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </div>
       {/* <Footer /> */}
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 // import HomeContent from "./home-content";
 // import Footer from "./footer";
 // import Hero from "./hero";
-// import Loading from "./loading";
+import Loading from "./loading";
 import NavBar from "./nav-bar";
 
 // export { HomeContent, Footer, Hero, Loading, NavBar };
-export { NavBar };
+export { NavBar, Loading };
